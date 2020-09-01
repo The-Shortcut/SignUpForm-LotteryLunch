b36b269876c24d830e232b291d63dbba7b4b73a8
Arifa <arifabegam.mujawar@gmail.com>
2020-09-01 12:37:39 +0300

    clear function, alert function

--- src/components/form.js
+++ src/components/form.js
@@ -1,88 +1,137 @@
 import React, { useState } from "react";
-import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn, MDBCard, MDBCardBody } from 'mdbreact';
-import axios from 'axios';
-import moment from 'moment';
+import {
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBInput,
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBAlert,
+} from "mdbreact";
+import axios from "axios";
+import moment from "moment";
 
 const FormPage = () => {
-    const [name, setName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail]= useState('');
-    const [confirmEmail, setConfirmEmail] = useState('');
-    const [date, setDate]= useState('');
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [confirmEmail, setConfirmEmail] = useState("");
+  const [date, setDate] = useState("");
+  const [error, setError] = useState("");
+  const handleRegister = (e) => {
+    e.preventDefault();
+    e.target.className += " was-validated";
+    console.log(
+      "name ",
+      name,
+      "email ",
+      email,
+      "c email ",
+      confirmEmail,
+      "date",
+      moment().format("YYYY-MM-DD")
+    );
 
-    const handleRegister = (e) => {
-        e.preventDefault();
-        console.log('name ', name, 'email ', email, 'c email ', confirmEmail,'date', moment().format('YYYY-MM-DD'))
-        
-        if(name && lastName && confirmEmail && date)
-        {
-          makePostRequest()
-        }else {
-          console.log('something is missing');
-        }
+    if (name && lastName && confirmEmail && date) {
+      makePostRequest();
+      clear();
+    } else {
+      console.log("something is missing");
+      console.log("check for email");
     }
+  };
 
-    const handleFirstNameChange = (e) =>{
-        //console.log('name ',e.target.value)
-        if(e.target.value){
-          setName(e.target.value)
-        }else {
-          console.log('Validation check')
-        }
-        
+  const handleFirstNameChange = (e) => {
+    //console.log('name ',e.target.value)
+    if (e.target.value) {
+      setName(e.target.value);
+    } else {
+      console.log("Validation check");
     }
+  };
 
-    const handleLastNameChange = (e) =>{
-      //console.log('name ',e.target.value)
-      if(e.target.value){
-      setLastName(e.target.value)
-    }else {
-      console.log('Validation check')
+  const handleLastNameChange = (e) => {
+    //console.log('name ',e.target.value)
+    if (e.target.value) {
+      setLastName(e.target.value);
+    } else {
+      console.log("Validation check");
     }
+  };
+
+  const handleEmailChange = (e) => {
+    // console.log('email ',e.target.value)
+    if (e.target.value) {
+      setEmail(e.target.value);
+    } else {
+      console.log("Validation check");
+    }
+  };
+
+  const handleConfirmEmailChange = (e) => {
+    // console.log('c email ',e.target.value)
+    if (email === e.target.value) {
+      console.log("emails match");
+      setConfirmEmail(e.target.value);
+      const temp = moment().format("YYYY-MM-DD");
+      setDate(temp);
+    } else {
+      console.log("emails, not a match");
+    }
+  };
+
+  const makePostRequest = async () => {
+    const data = {
+      email: email,
+      first_name: name,
+      last_name: lastName,
+      is_active: true,
+      join_at: date,
+    };
+    try {
+      let res = await axios.post(
+        "http://localhost:4000/api/addLotteryParticipants",
+        data
+      );
+      console.log(`Status code: ${res.status}`);
+      console.log(`Status text: ${res.statusText}`);
+    } catch (error) {
+      // console.log(error.response.status);
+      // if (error.response.status === 400) {
+        console.log("Username already exists!");
+        setError("Username already exists!!");
+      // }else {
+      //   console.log('falling here')
+      // }
+      clear();
+    }
+   
+  };
+  const clear = () => {
+   // setName('');
+    document.getElementById('fname').value=''
+    document.getElementById('lname').value=''
+    document.getElementById('email').value=''
+    document.getElementById('cemail').value=''
+    
+    //setLastName('');
+    // //setEmail('');
+    // setConfirmEmail('');
+    // setDate('');
+    // setError('');
   }
-
-    const handleEmailChange = (e) => {
-        // console.log('email ',e.target.value)
-        if(e.target.value){
-        setEmail(e.target.value)
-      }else {
-        console.log('Validation check')
-      }
-    }
-
-    const handleConfirmEmailChange = (e) => {
-        // console.log('c email ',e.target.value)
-        if(email === e.target.value){
-          console.log('emails match')
-          setConfirmEmail(e.target.value)
-          const temp = moment().format('YYYY-MM-DD');
-        setDate(temp)
-        }else{
-          console.log('emails, not a match')
-        }
-       
-    }
-
-    const makePostRequest = async () =>{
-      const data ={
-    "email":email,
-    "first_name":name, 
-    "last_name":lastName, 
-    "is_active":true,
-     "join_at":date
-      }
-        let res = await axios.post('http://localhost:4000/api/addLotteryParticipants', data)
-        console.log(`Status code: ${res.status}`);
-        console.log(`Status text: ${res.statusText}`);
-    }
   return (
-      
     <MDBContainer>
       <MDBRow>
         <MDBCol md="6">
           <MDBCard>
             <MDBCardBody>
-              <form>
+              <form
+                className="needs-validation"
+                onSubmit={(e) => handleRegister(e)}
+                noValidate
+              >
                 <p className="h4 text-center py-4">Sign up</p>
                 <div className="grey-text">
                   <MDBInput
@@ -94,14 +143,15 @@ const FormPage = () => {
                     required
                     error="wrong"
                     success="right"
-                    onChange={handleFirstNameChange}>
-                      <div className="invalid-feedback">
-                Please provide a valid first name.
-              </div>
-              <div className="valid-feedback">Looks good!</div>
-                    </MDBInput>
-                  
-                  
+                    id="fname"
+                    onChange={handleFirstNameChange}
+                  >
+                    <div className="invalid-feedback">
+                      Please provide a valid first name.
+                    </div>
+                    <div className="valid-feedback">Looks good!</div>
+                  </MDBInput>
+
                   <MDBInput
                     label="Your last name"
                     icon="user"
@@ -111,12 +161,13 @@ const FormPage = () => {
                     required
                     error="wrong"
                     success="right"
+                    id="lname"
                     onChange={handleLastNameChange}
-                    >
+                  >
                     <div className="invalid-feedback">
-              Please provide a valid last name.
-            </div>
-            <div className="valid-feedback">Looks good!</div>
+                      Please provide a valid last name.
+                    </div>
+                    <div className="valid-feedback">Looks good!</div>
                   </MDBInput>
                   <MDBInput
                     label="Your email"
@@ -127,12 +178,13 @@ const FormPage = () => {
                     required
                     error="wrong"
                     success="right"
+                    id="email"
                     onChange={handleEmailChange}
-                    >
+                  >
                     <div className="invalid-feedback">
-              Please provide a valid email.
-            </div>
-            <div className="valid-feedback">Looks good!</div>
+                      Please provide a valid email.
+                    </div>
+                    <div className="valid-feedback">Looks good!</div>
                   </MDBInput>
 
                   <MDBInput
@@ -144,30 +196,37 @@ const FormPage = () => {
                     required
                     error="wrong"
                     success="right"
+                    id="cemail"
                     onChange={handleConfirmEmailChange}
                   />
-                 
                 </div>
                 <MDBCol md="4" className="mb-3">
-            <div className="custom-control custom-checkbox pl-3">
-              <input
-                className="custom-control-input"
-                type="checkbox"
-                value=""
-                id="invalidCheck"
-                required
-              />
-              <label className="custom-control-label" htmlFor="invalidCheck">
-                Agree to terms and conditions
-              </label>
-              <div className="invalid-feedback">
-                You must agree before submitting.
-              </div>
-            </div>
-          </MDBCol>
-                
+                  <div className="custom-control custom-checkbox pl-3">
+                    <input
+                      className="custom-control-input"
+                      type="checkbox"
+                      value=""
+                      id="invalidCheck"
+                      required
+                    />
+                    <label
+                      className="custom-control-label"
+                      htmlFor="invalidCheck"
+                    >
+                      Agree to terms and conditions
+                    </label>
+                    <div className="invalid-feedback">
+                      You must agree before submitting.
+                    </div>
+                  </div>
+                </MDBCol>
+                {error && (
+                  <MDBAlert color="info" dismiss>
+                    {error}
+                  </MDBAlert>
+                )}
                 <div className="text-center py-4 mt-3">
-                  <MDBBtn color="cyan" type="submit" onClick={(e)=>handleRegister(e)}>
+                  <MDBBtn color="cyan" type="submit">
                     Register
                   </MDBBtn>
                 </div>
@@ -177,7 +236,6 @@ const FormPage = () => {
         </MDBCol>
       </MDBRow>
     </MDBContainer>
-   
   );
 };
 
